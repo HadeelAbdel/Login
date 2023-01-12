5246519200797f184c8ad2b2c1b78b8642696328
hadeel.khogali <hadeel.khogali@APR-ICT-14>
2023-01-12 14:46:42 +0200

    AdminLte

--- Login/Data/LoginContext.cs
+++ Login/Data/LoginContext.cs
@@ -26,8 +26,8 @@ namespace Login.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=Localhost;Database=Login;Trusted_Connection=True;");
+
+                
             }
         }
 
